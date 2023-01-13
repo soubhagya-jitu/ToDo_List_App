912d2cf31e34649ea2f5fc5c976c6f869cea8e13
soubhagya-jitu <soubhagyasamal03@gmail.com>
2023-01-13 20:21:19 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./App.css"
+import Items from "./items"
 
 const App = () => {
+
+  const [inputList, setInputList] = useState("")
+  const [item, setItem] = useState([])
+
+  const inputEvent = (event) => {
+    setInputList(event.target.value)
+  }
+
+  const setEvent = () => {
+    setItem((oldValue) => {
+      return [...oldValue, inputList]
+    })
+    setInputList("")
+  }
+
+  const deleteItems = (id)=> {
+    setItem((oldItems)=> {
+      return oldItems.filter((ele,index)=> {
+        return index!==id
+      })
+    })
+  }
   return (
-    <div>App</div>
+    <div>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a item" value={inputList} onChange={inputEvent} />
+          <button onClick={setEvent}> + </button>
+
+          <ol>
+              {item.map((itemValue,index) => { return <Items text={itemValue} key={index} id={index} delete={deleteItems}/>})}
+          </ol>
+        </div>
+      </div>
+    </div>
   )
 }
 
